ae5db121aca9afa8ae29409163ced52b30a97189
keerthisuryateja <keerthisuryateja2005@gmail.com>
2026-03-16 15:46:48 +0530

    Initial Mern CI Project

---
+++ server/test/app.test.js
@@ -0,0 +1,10 @@
+const request=require("supertest");
+const app=require("../index");
+describe("GET /",()=>{
+
+    it("should return Hello API!",async()=>{
+        const res=await request(app).get("/");
+        expect(res.statusCode).toEqual(200);
+       
+    });
+});
